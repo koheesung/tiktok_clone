fdb7da7ab37b5729306678344ed67381db03685e
koheesung <hs_ko@inplace.co.kr>
2023-10-30 12:28:26 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TilTok Colne',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFff2a55),
+      ),
+      home: Container(),
+    );
+  }
+}
